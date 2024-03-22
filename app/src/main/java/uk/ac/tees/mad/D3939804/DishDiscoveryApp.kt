0148194188929319615eae36e59aaa62a2097edd
Nikhil Kotnala <nikhilkotnala01@gmail.com>
2024-03-22 08:58:44 +0000

    -- Added HiltModule, IgViewModel

---
+++ app/src/main/java/uk/ac/tees/mad/D3939804/DishDiscoveryApp.kt
@@ -0,0 +1,8 @@
+package uk.ac.tees.mad.D3939804
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class DishDiscoveryApp: Application() {
+}
--- app/src/main/java/uk/ac/tees/mad/D3939804/MainActivity.kt
+++ app/src/main/java/uk/ac/tees/mad/D3939804/MainActivity.kt
@@ -12,12 +12,14 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
+import dagger.hilt.android.AndroidEntryPoint
 import uk.ac.tees.mad.D3939804.ui.theme.ComposeLoginTheme
 
 import uk.ac.tees.mad.D3939804.ui.screens.NavigationRoutes
 import uk.ac.tees.mad.D3939804.ui.screens.authenticatedGraph
 import uk.ac.tees.mad.D3939804.ui.screens.unauthenticatedGraph
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
